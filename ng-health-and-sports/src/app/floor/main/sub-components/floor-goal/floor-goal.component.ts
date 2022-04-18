44ac02904d344a5988aaab95d465aa5c79b7d49c
Ivan <Ivan_567.92@mail.ru>
2022-04-19 01:28:11 +0300

    work layout

---
+++ ng-health-and-sports/src/app/floor/main/sub-components/floor-goal/floor-goal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'floor-goal',
+  templateUrl: './floor-goal.component.html',
+  styleUrls: ['./floor-goal.component.scss']
+})
+export class FloorGoalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
